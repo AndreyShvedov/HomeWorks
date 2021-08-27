2ae33302e5d2095c1d3e4118a61e0ef7e786572c
AndreyShvedov <tushcanchik1@gmail.com>
2021-08-27 23:41:42 +0300

    3 commit

--- src/phonebook/storage/DatabaseStorage.java
+++ src/phonebook/storage/DatabaseStorage.java
@@ -5,6 +5,5 @@ import phonebook.entity.Person;
 public class DatabaseStorage implements phonebook.storage.Storage {
     @Override
     public void save(Person save) {
-        ;
     }
 }
---
+++ src/phonebook/storage/Storage.java
@@ -0,0 +1,9 @@
+package phonebook.storage;
+
+import phonebook.entity.Person;
+
+public interface Storage {
+    default void save(Person save) {
+
+    }
+}
