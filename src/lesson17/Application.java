14f50578902a2f5e055147eba06f041dd58cafc0
Andrei Shvedov <tushcanchik1@gmail.com>
2021-10-09 22:08:42 +0300

    lesson17

---
+++ src/lesson17/Application.java
@@ -0,0 +1,8 @@
+package lesson17;
+
+public class Application {
+    public static void main(String[] args) {
+        var thread = new MyThread();
+        thread.start();
+    }
+}
---
+++ src/lesson17/MyThread.java
@@ -0,0 +1,22 @@
+package lesson17;
+
+import java.io.*;
+
+public class MyThread extends Thread {
+    public void run() {
+        File file = new File("d:\\Study\\Java\\HomeWorks\\out\\production\\HomeWorks\\phonebook\\");
+        File[] files = file.listFiles(((dir, name) -> name.endsWith(".class")));
+
+        for (File value : files) {
+            try (FileInputStream fis = new FileInputStream(value);
+                 FileOutputStream fos = new FileOutputStream("thread.txt", false)) {
+                byte[] buffer = new byte[fis.available()];
+                fis.read(buffer, 0, buffer.length);
+                fos.write(buffer, 0, buffer.length);
+            } catch (IOException ex) {
+
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
